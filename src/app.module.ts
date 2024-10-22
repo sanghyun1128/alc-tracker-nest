07032c126cea4f4edb2cca0af5e24938aaf894c7
sanghyun1128 <sanghyun1128@gmail.com>
2024-10-22 16:34:31 +0900

    refactor: Update app.module.ts to include TypeORM and ConfigModule dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { SpiritsModule } from './spirits/spirits.module';
 import { WinesModule } from './wines/wines.module';
 import { CocktailsModule } from './cocktails/cocktails.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -14,6 +16,20 @@ import { UsersModule } from './users/users.module';
     WinesModule,
     CocktailsModule,
     UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
