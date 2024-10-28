a7229db13d9619c84a4ef73a33284e86362d8be5
sanghyun1128 <sanghyun1128@gmail.com>
2024-10-28 17:21:28 +0900

    feat: add getAllReviews and getReviewById endpoints

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
+
+  @Get()
+  async getAllReviews() {
+    return this.reviewsService.getAllReviews();
+  }
+
+  @Get(':id')
+  async getReviewById(@Param('id') id: string) {
+    return this.reviewsService.getReviewById(+id);
+  }
 }
