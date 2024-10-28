ca4a74348246612001a6d0ea190d14b307d18804
sanghyun1128 <sanghyun1128@gmail.com>
2024-10-28 17:22:01 +0900

    feat: Add getAllReviews and getReviewById logics

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ReviewModel } from './entities/review.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class ReviewsService {}
+export class ReviewsService {
+  constructor(
+    @InjectRepository(ReviewModel)
+    private readonly reviewRepository: Repository<ReviewModel>,
+  ) {}
+
+  async getAllReviews() {
+    return this.reviewRepository.find();
+  }
+
+  async getReviewById(id: number) {
+    const review = await this.reviewRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!review) {
+      throw new NotFoundException(`Review with id ${id} not found`);
+    }
+
+    return review;
+  }
+}
