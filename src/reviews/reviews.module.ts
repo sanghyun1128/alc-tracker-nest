77a14b3da9cd37ee079a1cd8f76445eda0568855
sanghyun1128 <sanghyun1128@gmail.com>
2024-10-28 17:20:37 +0900

    feat: Add TypeOrmModule import for ReviewModel entity in reviews.module.ts

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReviewModel } from './entities/review.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReviewModel])],
   controllers: [ReviewsController],
   providers: [ReviewsService],
 })
