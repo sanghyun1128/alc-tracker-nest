bc1f57013c34abd3f416821724eae0fc775acff9
sanghyun1128 <sanghyun1128@gmail.com>
2024-11-02 16:45:52 +0900

    feat: Add CocktailCategoryEnum for categorizing different cocktail types

---
+++ src/alcohol/const/cocktail.const.ts
@@ -0,0 +1,13 @@
+export enum CocktailCategoryEnum {
+  CLASSIC = 'classic',
+  FRUIT = 'fruit',
+  SPARKLING = 'sparkling',
+  SMOOTHIE_SHAKE = 'smoothie_shake',
+  MOCKTAIL = 'mocktail',
+  ON_THE_ROCKS = 'on_the_rocks',
+  HERB_SPICE = 'herb_spice',
+  SUMMER = 'summer',
+  WINTER = 'winter',
+  DESSERT = 'dessert',
+  FUSION = 'fusion',
+}
