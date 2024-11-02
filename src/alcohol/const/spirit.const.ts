06186cd0cf7aca0109759b0cf8de7a1ce609f0c6
sanghyun1128 <sanghyun1128@gmail.com>
2024-11-02 16:32:26 +0900

    feat: Add SpiritCategoryEnum and CaskEnum for categorizing spirits and cask types

---
+++ src/alcohol/const/spirit.const.ts
@@ -0,0 +1,31 @@
+export enum SpiritCategoryEnum {
+  Irish = 'irish',
+  Scotch = 'scotch',
+  Bourbon = 'bourbon',
+  WHISKY = 'whisky',
+  VODKA = 'vodka',
+  GIN = 'gin',
+  RUM = 'rum',
+  TEQUILA = 'tequila',
+  COGNAC = 'cognac',
+  ARMAGNAC = 'armagnac',
+  CALVADOS = 'calvados',
+  BRANDY = 'brandy',
+  LIQUEUR = 'liqueur',
+  SOJU = 'soju',
+  MAKGEOLLI = 'makgeolli',
+  TRADITIONAL = 'traditional',
+  OTHER = 'other',
+}
+
+export enum CaskEnum {
+  BOURBON = 'bourbon',
+  SHERRY = 'sherry',
+  WINE = 'wine',
+  PORT = 'port',
+  MADEIRA = 'madeira',
+  RUM = 'rum',
+  BEER = 'beer',
+  VIRGIN = 'virgin',
+  OTHER = 'other',
+}
