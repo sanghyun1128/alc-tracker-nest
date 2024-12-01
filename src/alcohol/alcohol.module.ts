c3105c44db522b7d6ed2fddace328761475e4ae3
sanghyun1128 <sanghyun1128@gmail.com>
2024-12-01 19:39:23 +0900

    feat: Integrate TypeOrmModule for alcohol entities in AlcoholModule

--- src/alcohol/alcohol.module.ts
+++ src/alcohol/alcohol.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AlcoholController } from './alcohol.controller';
 import { AlcoholService } from './alcohol.service';
+import {
+  AlcoholModel,
+  CocktailModel,
+  SpiritModel,
+  WineModel,
+} from './entities/alcohol.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      AlcoholModel,
+      SpiritModel,
+      WineModel,
+      CocktailModel,
+    ]),
+  ],
   controllers: [AlcoholController],
   providers: [AlcoholService],
 })
