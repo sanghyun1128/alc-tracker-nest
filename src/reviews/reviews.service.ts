70285968951b2b8512b2942377ec002f8f0bec30
sanghyun1128 <sanghyun1128@gmail.com>
2024-12-01 19:32:00 +0900

    feat: Add functions for Get and Post reviews

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -3,7 +3,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import {
+  BaseReviewModel,
   CocktailReviewModel,
+  DetailEvaluation,
   SpiritReviewModel,
   WineReviewModel,
 } from './entities/review.entity';
@@ -11,16 +13,26 @@ import {
 @Injectable()
 export class ReviewsService {
   constructor(
+    @InjectRepository(BaseReviewModel)
+    private readonly reviewRepository: Repository<BaseReviewModel>,
     @InjectRepository(SpiritReviewModel)
     private readonly spiritReviewRepository: Repository<SpiritReviewModel>,
     @InjectRepository(WineReviewModel)
     private readonly wineReviewRepository: Repository<WineReviewModel>,
     @InjectRepository(CocktailReviewModel)
-    private readonly reviewRepository: Repository<CocktailReviewModel>,
+    private readonly cocktailReviewRepository: Repository<CocktailReviewModel>,
   ) {}
 
-  async getAllReviews() {
-    return this.reviewRepository.find();
+  async getAllSpiritReviews() {
+    return this.spiritReviewRepository.find();
+  }
+
+  async getAllWineReviews() {
+    return this.wineReviewRepository.find();
+  }
+
+  async getAllCocktailReviews() {
+    return this.cocktailReviewRepository.find();
   }
 
   async getReviewById(id: number) {
@@ -37,14 +49,77 @@ export class ReviewsService {
     return review;
   }
 
-  async createReview() {
-    const review = this.reviewRepository.create({
-      rating: 5,
-      comment: 'Great product!',
+  async createSpiritReview(
+    rating: number,
+    comment: string,
+    pairing: string,
+    nose: DetailEvaluation,
+    palate: DetailEvaluation,
+    finish: DetailEvaluation,
+    bottleCondition: number,
+  ) {
+    const review = this.spiritReviewRepository.create({
+      rating,
+      comment,
+      pairing,
+      nose,
+      palate,
+      finish,
+      bottleCondition,
     });
 
-    const newReview = await this.reviewRepository.save(review);
+    const spiritReview = this.spiritReviewRepository.save(review);
 
-    return newReview;
+    return spiritReview;
+  }
+
+  async createWineReview(
+    rating: number,
+    comment: string,
+    pairing: string,
+    nose: DetailEvaluation,
+    palate: DetailEvaluation,
+    finish: DetailEvaluation,
+    aeration: number,
+  ) {
+    const review = this.wineReviewRepository.create({
+      rating,
+      comment,
+      pairing,
+      nose,
+      palate,
+      finish,
+      aeration,
+    });
+
+    const wineReview = this.wineReviewRepository.save(review);
+
+    return wineReview;
+  }
+
+  async createCocktailReview(
+    rating: number,
+    comment: string,
+    pairing: string,
+    nose: DetailEvaluation,
+    palate: DetailEvaluation,
+    finish: DetailEvaluation,
+    ingredients: string,
+    recipe: string,
+  ) {
+    const review = this.cocktailReviewRepository.create({
+      rating,
+      comment,
+      pairing,
+      nose,
+      palate,
+      finish,
+      ingredients,
+      recipe,
+    });
+
+    const cocktailReview = this.cocktailReviewRepository.save(review);
+
+    return cocktailReview;
   }
 }
