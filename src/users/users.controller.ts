e78bcbf1d3cdbc5cd986afc63ba70eb227cd3dd9
sanghyun1128 <sanghyun1128@gmail.com>
2024-12-01 17:02:37 +0900

    feat: Implement user creation and retrieval in UsersController and UsersService

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  postUser(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Body('nickname') nickname: string,
+    @Body('birth') birth: Date,
+  ) {
+    return this.usersService.createUser(nickname, email, password, birth);
+  }
+
+  //FIXME: test code
+  @Get('all')
+  getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { UserModel } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserModel)
+    private readonly usersRepository: Repository<UserModel>,
+  ) {}
+
+  async createUser(
+    nickname: string,
+    email: string,
+    password: string,
+    birth: Date,
+  ) {
+    const user = this.usersRepository.create({
+      nickname,
+      email,
+      password,
+      birth,
+    });
+
+    return this.usersRepository.save(user);
+  }
+
+  //FIXME: test code
+  async getAllUsers() {
+    return this.usersRepository.find();
+  }
+}
