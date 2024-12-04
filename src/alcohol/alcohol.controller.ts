c79453e3f2a484df6342cfbff42ab0af0680920f
sanghyun1128 <sanghyun1128@gmail.com>
2024-12-04 17:57:42 +0900

    feat: Implement endpoints for retrieving and creating spirits, wines, and cocktails in AlcoholController and AlcoholService

--- src/alcohol/alcohol.controller.ts
+++ src/alcohol/alcohol.controller.ts
@@ -1,8 +1,96 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 import { AlcoholService } from './alcohol.service';
+import { CocktailCategoryEnum } from './const/cocktail.const';
+import { CaskEnum, SpiritCategoryEnum } from './const/spirit.const';
+import {
+  CombinedAppellationType,
+  GrapeVarietyEnum,
+  WineCategoryEnum,
+  WineRegionEnum,
+} from './const/wine.const';
 
 @Controller('alcohol')
 export class AlcoholController {
   constructor(private readonly alcoholService: AlcoholService) {}
+
+  @Get('/spirit')
+  getAllSpirits() {
+    return this.alcoholService.getAllSpirits();
+  }
+
+  @Get('/wine')
+  getAllWines() {
+    return this.alcoholService.getAllWines();
+  }
+
+  @Get('/cocktail')
+  getAllCocktails() {
+    return this.alcoholService.getAllCocktails();
+  }
+
+  @Get(':id')
+  getAlcoholById(@Param('id') id: string) {
+    return this.alcoholService.getAlcoholById(+id);
+  }
+
+  @Post('/spirit')
+  createSpirit(
+    @Body('name') name: string,
+    @Body('category') category: SpiritCategoryEnum,
+    @Body('cask') cask: CaskEnum,
+    @Body('maker') maker: string,
+    @Body('alc') alc: number,
+    @Body('price') price: number,
+    @Body('purchaseLocation') purchaseLocation: string,
+    @Body('purchaseDate') purchaseDate: Date,
+  ) {
+    return this.alcoholService.createSpirit(
+      name,
+      category,
+      cask,
+      maker,
+      alc,
+      price,
+      purchaseLocation,
+      purchaseDate,
+    );
+  }
+
+  @Post('/wine')
+  createWine(
+    @Body('name') name: string,
+    @Body('category') category: WineCategoryEnum,
+    @Body('region') region: WineRegionEnum,
+    @Body('appellation') appellation: CombinedAppellationType,
+    @Body('grape') grape: GrapeVarietyEnum,
+    @Body('vintage') vintage: number,
+    @Body('maker') maker: string,
+    @Body('alc') alc: number,
+    @Body('price') price: number,
+    @Body('purchaseLocation') purchaseLocation: string,
+    @Body('purchaseDate') purchaseDate: Date,
+  ) {
+    return this.alcoholService.createWine(
+      name,
+      category,
+      region,
+      appellation,
+      grape,
+      vintage,
+      maker,
+      alc,
+      price,
+      purchaseLocation,
+      purchaseDate,
+    );
+  }
+
+  @Post('/cocktail')
+  createCocktail(
+    @Body('name') name: string,
+    @Body('category') category: CocktailCategoryEnum,
+  ) {
+    return this.alcoholService.createCocktail(name, category);
+  }
 }
--- src/alcohol/alcohol.service.ts
+++ src/alcohol/alcohol.service.ts
@@ -1,4 +1,127 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { CocktailCategoryEnum } from './const/cocktail.const';
+import { CaskEnum, SpiritCategoryEnum } from './const/spirit.const';
+import {
+  CombinedAppellationType,
+  GrapeVarietyEnum,
+  WineCategoryEnum,
+  WineRegionEnum,
+} from './const/wine.const';
+import {
+  AlcoholModel,
+  CocktailModel,
+  SpiritModel,
+  WineModel,
+} from './entities/alcohol.entity';
 
 @Injectable()
-export class AlcoholService {}
+export class AlcoholService {
+  constructor(
+    @InjectRepository(AlcoholModel)
+    private readonly alcoholRepository: Repository<AlcoholModel>,
+    @InjectRepository(SpiritModel)
+    private readonly spiritRepository: Repository<SpiritModel>,
+    @InjectRepository(WineModel)
+    private readonly wineRepository: Repository<WineModel>,
+    @InjectRepository(CocktailModel)
+    private readonly cocktailRepository: Repository<CocktailModel>,
+  ) {}
+
+  async getAllSpirits() {
+    return this.spiritRepository.find();
+  }
+
+  async getAllWines() {
+    return this.wineRepository.find();
+  }
+
+  async getAllCocktails() {
+    return this.cocktailRepository.find();
+  }
+
+  async getAlcoholById(id: number) {
+    const alcohol = await this.alcoholRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!alcohol) {
+      throw new NotFoundException(`Alcohol with id ${id} not found`);
+    }
+
+    return alcohol;
+  }
+
+  async createSpirit(
+    name: string,
+    category: SpiritCategoryEnum,
+    cask: CaskEnum,
+    maker: string,
+    alc: number,
+    price: number,
+    purchaseLocation: string,
+    purchaseDate: Date,
+  ) {
+    const alcohol = this.spiritRepository.create({
+      name,
+      category,
+      cask,
+      maker,
+      alc,
+      price,
+      purchaseLocation,
+      purchaseDate,
+    });
+
+    const spirit = this.spiritRepository.save(alcohol);
+
+    return spirit;
+  }
+
+  async createWine(
+    name: string,
+    category: WineCategoryEnum,
+    region: WineRegionEnum,
+    appellation: CombinedAppellationType,
+    grape: GrapeVarietyEnum,
+    vintage: number,
+    maker: string,
+    alc: number,
+    price: number,
+    purchaseLocation: string,
+    purchaseDate: Date,
+  ) {
+    const alcohol = this.wineRepository.create({
+      name,
+      category,
+      region,
+      appellation,
+      grape,
+      vintage,
+      maker,
+      alc,
+      price,
+      purchaseLocation,
+      purchaseDate,
+    });
+
+    const wine = this.wineRepository.save(alcohol);
+
+    return wine;
+  }
+
+  async createCocktail(name: string, category: CocktailCategoryEnum) {
+    const alcohol = this.cocktailRepository.create({
+      name,
+      category,
+    });
+
+    const cocktail = this.cocktailRepository.save(alcohol);
+
+    return cocktail;
+  }
+}
