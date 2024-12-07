53729f974f15f71ce9c6595c8d64f5c5aa8b3b1b
sanghyun1128 <sanghyun1128@gmail.com>
2024-12-08 00:47:06 +0900

    refactor: remove unused postUser method from UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,20 +1,10 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  postUser(
-    @Body('email') email: string,
-    @Body('password') password: string,
-    @Body('nickname') nickname: string,
-    @Body('birth') birth: Date,
-  ) {
-    return this.usersService.createUser(nickname, email, password, birth);
-  }
 
   //FIXME: test code
   @Get('all')
