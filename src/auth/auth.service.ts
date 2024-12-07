1c67caee8b31df546063ea321e626e21237353db
sanghyun1128 <sanghyun1128@gmail.com>
2024-12-08 00:55:12 +0900

    feat: implement registerWithEmail method in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,14 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 
+import {
+  ENV_JWT_ACCESS_TOKEN_EXPIRATION,
+  ENV_JWT_HASH_ROUNDS_KEY,
+  ENV_JWT_REFRESH_TOKEN_EXPIRATION,
+  ENV_JWT_SECRET_KEY,
+} from 'src/common/const/env-keys.const';
 import { UserModel } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 
@@ -10,7 +17,34 @@ export class AuthService {
   constructor(
     private readonly jwtService: JwtService,
     private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
   ) {}
+
+  /**
+   * Register a new user with email, password, nickname, birth date, and gender.
+   *
+   * @param user - An object containing the user's email, password, nickname, birth date, and gender.
+   * @returns An object containing the access token and refresh token.
+   * @throws BadRequestException if the email or nickname already exists.
+   */
+  async registerWithEmail(
+    user: Pick<
+      UserModel,
+      'nickname' | 'email' | 'password' | 'birth' | 'gender'
+    >,
+  ) {
+    const hashedPassword = await bcrypt.hash(
+      user.password,
+      +this.configService.get<string>(ENV_JWT_HASH_ROUNDS_KEY),
+    );
+
+    const newUser = await this.usersService.createUser({
+      ...user,
+      password: hashedPassword,
+    });
+
+    return this.loginUser(newUser);
+  }
 
   /**
    * Login a user with email and password.
@@ -59,10 +93,10 @@ export class AuthService {
     };
 
     return this.jwtService.sign(payload, {
-      secret: process.env.JWT_SECRET,
+      secret: this.configService.get<string>(ENV_JWT_SECRET_KEY),
       expiresIn: isRefreshToken
-        ? process.env.JWT_REFRESH_TOKEN_EXPIRATION
-        : process.env.JWT_ACCESS_TOKEN_EXPIRATION,
+        ? +this.configService.get<string>(ENV_JWT_REFRESH_TOKEN_EXPIRATION)
+        : +this.configService.get<string>(ENV_JWT_ACCESS_TOKEN_EXPIRATION),
     });
   }
 }
