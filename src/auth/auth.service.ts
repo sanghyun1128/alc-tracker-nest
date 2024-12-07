5cf9d8efc35647cbd6baa9b78c7a55410dd0c9c3
sanghyun1128 <sanghyun1128@gmail.com>
2024-12-07 16:53:22 +0900

    feat: implement email and password authentication in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 import { UserModel } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
   /**
    * TODO: register with email
    *  - email, pw, nickname...
@@ -34,5 +39,31 @@ export class AuthService {
         ? process.env.JWT_REFRESH_TOKEN_EXPIRATION
         : process.env.JWT_ACCESS_TOKEN_EXPIRATION,
     });
+  }
+
+  loginUser(user: Pick<UserModel, 'email' | 'id'>) {
+    return {
+      accessToken: this.signToken(user, false),
+      refreshToken: this.signToken(user, true),
+    };
+  }
+
+  async authWithEmailAndPassword(user: Pick<UserModel, 'email' | 'password'>) {
+    const existingUser = await this.usersService.getUserByEmail(user.email);
+
+    if (!existingUser) {
+      throw new UnauthorizedException('Not existing user');
+    }
+
+    const isPasswordMatched = await bcrypt.compare(
+      user.password,
+      existingUser.password,
+    );
+
+    if (!isPasswordMatched) {
+      throw new UnauthorizedException('Password is not matched');
+    }
+
+    return existingUser;
   }
 }
