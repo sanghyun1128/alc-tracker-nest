3f27ef0822b1fe36cb3a16b14d28a9fdacd6ab28
sanghyun1128 <sanghyun1128@gmail.com>
2025-01-12 13:46:42 +0900

    feat: add pagination DTO and order enum for alcohol retrieval

---
+++ src/alcohol/dto/paginate-alcohol.dto.ts
@@ -0,0 +1,26 @@
+import { Type } from 'class-transformer';
+import { IsDate, IsEnum, IsNumber, IsOptional } from 'class-validator';
+
+import { OrderEnum } from '../const/pagination.const';
+import {
+  dateValidationMessage,
+  enumValidationMessage,
+  numberValidationMessage,
+} from 'src/common/validation-message';
+
+export class PaginateAlcoholDto {
+  @IsDate({ message: dateValidationMessage })
+  @Type(() => Date)
+  @IsOptional()
+  where__createdAt_more_than?: Date;
+
+  @IsEnum(OrderEnum, {
+    message: enumValidationMessage,
+  })
+  @IsOptional()
+  order__createdAt?: OrderEnum = OrderEnum.ASC;
+
+  @IsNumber({}, { message: numberValidationMessage })
+  @IsOptional()
+  take: number = 10;
+}
