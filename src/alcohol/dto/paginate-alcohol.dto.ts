560628d79c2146fc6a451c19b9a2911dbdf197d0
sanghyun1128 <sanghyun1128@gmail.com>
2025-01-14 22:48:45 +0900

    feat: extend PaginateAlcoholDto from BasePaginationDto

--- src/alcohol/dto/paginate-alcohol.dto.ts
+++ src/alcohol/dto/paginate-alcohol.dto.ts
@@ -1,24 +1,3 @@
-import { IsEnum, IsNumber, IsOptional } from 'class-validator';
+import { BasePaginationDto } from 'src/common/dto/base-pagination.dto';
 
-import { OrderEnum } from '../const/pagination.const';
-import { enumValidationMessage, numberValidationMessage } from 'src/common/validation-message';
-
-export class PaginateAlcoholDto {
-  @IsNumber({}, { message: numberValidationMessage })
-  @IsOptional()
-  page?: number;
-
-  @IsNumber({}, { message: numberValidationMessage })
-  @IsOptional()
-  where__cursor?: number;
-
-  @IsEnum(OrderEnum, {
-    message: enumValidationMessage,
-  })
-  @IsOptional()
-  order__createdAt?: OrderEnum = OrderEnum.ASC;
-
-  @IsNumber({}, { message: numberValidationMessage })
-  @IsOptional()
-  take: number = 10;
-}
+export class PaginateAlcoholDto extends BasePaginationDto {}
