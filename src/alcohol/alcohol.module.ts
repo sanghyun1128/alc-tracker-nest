d753987d300fa6efc17658d332ccd6c29e58eca8
sanghyun1128 <sanghyun1128@gmail.com>
2025-01-14 22:49:12 +0900

    feat: add CommonModule to AlcoholModule imports

--- src/alcohol/alcohol.module.ts
+++ src/alcohol/alcohol.module.ts
@@ -5,10 +5,11 @@ import { AlcoholController } from './alcohol.controller';
 import { AlcoholService } from './alcohol.service';
 import { CocktailModel, SpiritModel, WineModel } from './entities/alcohol.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { CommonModule } from 'src/common/common.module';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SpiritModel, WineModel, CocktailModel]), AuthModule, UsersModule],
+  imports: [TypeOrmModule.forFeature([SpiritModel, WineModel, CocktailModel]), AuthModule, UsersModule, CommonModule],
   controllers: [AlcoholController],
   providers: [AlcoholService],
 })
