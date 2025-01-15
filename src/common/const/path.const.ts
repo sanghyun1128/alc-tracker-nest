a1251affd81ece412923746f633d1e1658165e43
sanghyun1128 <sanghyun1128@gmail.com>
2025-01-15 20:56:20 +0900

    feat: add path constants for project structure and domain folders

---
+++ src/common/const/path.const.ts
@@ -0,0 +1,28 @@
+import { join } from 'path';
+
+// The root path of the project
+export const PROJECT_ROOT_PATH = process.cwd();
+
+// The name of the public folder
+export const PUBLIC_FOLDER_NAME = 'public';
+
+// The names of the folders for each domain
+export const ALCOHOLS_FOLDER_NAME = 'alcohols';
+export const REVIEWS_FOLDER_NAME = 'reviews';
+export const USERS_FOLDER_NAME = 'users';
+
+// The name of the images folder
+export const IMAGES_FOLDER_NAME = 'images';
+
+// The path to the public folder
+export const PUBLIC_FOLDER_PATH = join(PROJECT_ROOT_PATH, PUBLIC_FOLDER_NAME);
+
+// The paths to the folders for each domain
+export const ALCOHOLS_FOLDER_PATH = join(PUBLIC_FOLDER_PATH, ALCOHOLS_FOLDER_NAME);
+export const REVIEWS_FOLDER_PATH = join(PUBLIC_FOLDER_PATH, REVIEWS_FOLDER_NAME);
+export const USERS_FOLDER_PATH = join(PUBLIC_FOLDER_PATH, USERS_FOLDER_NAME);
+
+// The paths to the images folders for each domain
+export const ALCOHOLS_IMAGES_FOLDER_PATH = join(ALCOHOLS_FOLDER_PATH, IMAGES_FOLDER_NAME);
+export const REVIEWS_IMAGES_FOLDER_PATH = join(REVIEWS_FOLDER_PATH, IMAGES_FOLDER_NAME);
+export const USERS_IMAGES_FOLDER_PATH = join(USERS_FOLDER_PATH, IMAGES_FOLDER_NAME);
