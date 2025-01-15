ad4cf1380753d3ac4cad0b4ae5c2ba47d04f5737
sanghyun1128 <sanghyun1128@gmail.com>
2025-01-15 20:17:53 +0900

    feat: add optional string filter for name in PaginateAlcoholDto

--- src/alcohol/dto/paginate-alcohol.dto.ts
+++ src/alcohol/dto/paginate-alcohol.dto.ts
@@ -1,3 +1,10 @@
-import { BasePaginationDto } from 'src/common/dto/base-pagination.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class PaginateAlcoholDto extends BasePaginationDto {}
+import { BasePaginationDto } from 'src/common/dto/base-pagination.dto';
+import { stringValidationMessage } from 'src/common/validation-message';
+
+export class PaginateAlcoholDto extends BasePaginationDto {
+  @IsString({ message: stringValidationMessage })
+  @IsOptional()
+  where__name__i_like?: string;
+}
