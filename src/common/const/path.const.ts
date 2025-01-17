991dedb22121d786dfbb8d17285c37e233363ab0
sanghyun1128 <sanghyun1128@gmail.com>
2025-01-17 22:49:52 +0900

    feat: add TEMP_FOLDER_NAME constant and TEMP_FOLDER_PATH for temporary storage

--- src/common/const/path.const.ts
+++ src/common/const/path.const.ts
@@ -6,6 +6,9 @@ export const PROJECT_ROOT_PATH = process.cwd();
 // The name of the public folder
 export const PUBLIC_FOLDER_NAME = 'public';
 
+// The name of temporary folder
+export const TEMP_FOLDER_NAME = 'temp';
+
 // The names of the folders for each domain
 export const ALCOHOLS_FOLDER_NAME = 'alcohols';
 export const REVIEWS_FOLDER_NAME = 'reviews';
@@ -26,3 +29,6 @@ export const USERS_FOLDER_PATH = join(PUBLIC_FOLDER_PATH, USERS_FOLDER_NAME);
 export const ALCOHOLS_IMAGES_FOLDER_PATH = join(ALCOHOLS_FOLDER_PATH, IMAGES_FOLDER_NAME);
 export const REVIEWS_IMAGES_FOLDER_PATH = join(REVIEWS_FOLDER_PATH, IMAGES_FOLDER_NAME);
 export const USERS_IMAGES_FOLDER_PATH = join(USERS_FOLDER_PATH, IMAGES_FOLDER_NAME);
+
+// The path to the temporary folder
+export const TEMP_FOLDER_PATH = join(PROJECT_ROOT_PATH, TEMP_FOLDER_NAME);
