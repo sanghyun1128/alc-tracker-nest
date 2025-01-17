2533d9664048a01f4dcb12c0b2bf9e3338c57652
sanghyun1128 <sanghyun1128@gmail.com>
2025-01-17 22:50:06 +0900

    feat: move multer configuration to CommonModule

--- src/alcohol/alcohol.module.ts
+++ src/alcohol/alcohol.module.ts
@@ -1,52 +1,15 @@
 import { Module } from '@nestjs/common';
-import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import * as multer from 'multer';
-import { extname } from 'path';
-import { v4 as uuidV4 } from 'uuid';
 
 import { AlcoholController } from './alcohol.controller';
 import { AlcoholService } from './alcohol.service';
 import { CocktailModel, SpiritModel, WineModel } from './entities/alcohol.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { CommonModule } from 'src/common/common.module';
-import { ALCOHOLS_IMAGES_FOLDER_PATH } from 'src/common/const/path.const';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([SpiritModel, WineModel, CocktailModel]),
-    AuthModule,
-    UsersModule,
-    CommonModule,
-    MulterModule.register({
-      limits: {
-        fileSize: 1024 * 1024 * 10, // 10MB
-      },
-      fileFilter: (req, file, cb) => {
-        const ext = extname(file.originalname);
-
-        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
-          return cb(new Error('Only jpg, jpeg and png is allowed'), false);
-        }
-
-        return cb(null, true);
-      },
-      storage: multer.diskStorage({
-        destination: function (req, file, cb) {
-          cb(null, ALCOHOLS_IMAGES_FOLDER_PATH);
-        },
-        filename: function (req, file, cb) {
-          const date = new Date();
-          const year = date.getFullYear();
-          const month = String(date.getMonth() + 1).padStart(2, '0');
-          const day = String(date.getDate()).padStart(2, '0');
-
-          cb(null, `${year}${month}${day}-${uuidV4()}${extname(file.originalname)}`);
-        },
-      }),
-    }),
-  ],
+  imports: [TypeOrmModule.forFeature([SpiritModel, WineModel, CocktailModel]), AuthModule, UsersModule, CommonModule],
   controllers: [AlcoholController],
   providers: [AlcoholService],
 })
--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,9 +1,46 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import * as multer from 'multer';
+import { extname } from 'path';
+import { v4 as uuidV4 } from 'uuid';
 
 import { CommonController } from './common.controller';
 import { CommonService } from './common.service';
-
+import { TEMP_FOLDER_PATH } from './const/path.const';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 @Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    MulterModule.register({
+      limits: {
+        fileSize: 1024 * 1024 * 10, // 10MB
+      },
+      fileFilter: (req, file, cb) => {
+        const ext = extname(file.originalname);
+
+        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+          return cb(new Error('Only jpg, jpeg and png is allowed'), false);
+        }
+
+        return cb(null, true);
+      },
+      storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, TEMP_FOLDER_PATH);
+        },
+        filename: function (req, file, cb) {
+          const date = new Date();
+          const year = date.getFullYear();
+          const month = String(date.getMonth() + 1).padStart(2, '0');
+          const day = String(date.getDate()).padStart(2, '0');
+
+          cb(null, `${year}${month}${day}-${uuidV4()}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+  ],
   controllers: [CommonController],
   providers: [CommonService],
   exports: [CommonService],
