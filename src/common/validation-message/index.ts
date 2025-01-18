046dba90c88dff9063ec812186b75aa3704421c4
sanghyun1128 <sanghyun1128@gmail.com>
2025-01-19 02:07:30 +0900

    feat: add integer validation message to number validation module

--- src/common/validation-message/index.ts
+++ src/common/validation-message/index.ts
@@ -2,5 +2,5 @@ export { dateValidationMessage } from './date-validation.message';
 export { emailValidationMessage } from './email-validation.message';
 export { enumValidationMessage } from './enum-validation.message';
 export { lengthValidationMessage } from './length-validation.message';
-export { numberValidationMessage } from './number-validation.message';
+export { numberValidationMessage, integerValidationMessage } from './number-validation.message';
 export { stringValidationMessage } from './string-validation.message';
--- src/common/validation-message/number-validation.message.ts
+++ src/common/validation-message/number-validation.message.ts
@@ -3,3 +3,7 @@ import { ValidationArguments } from 'class-validator';
 export const numberValidationMessage = (args: ValidationArguments) => {
   return `${args.property} is not number`;
 };
+
+export const integerValidationMessage = (args: ValidationArguments) => {
+  return `${args.property} is not integer`;
+};
