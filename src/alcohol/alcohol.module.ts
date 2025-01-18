3f21c17eecab155c5b24b3cabd3b0e248592b7d3
sanghyun1128 <sanghyun1128@gmail.com>
2025-01-19 03:04:51 +0900

    feat: include AlcoholModel and ImageModel in alcohol module imports

--- src/alcohol/alcohol.module.ts
+++ src/alcohol/alcohol.module.ts
@@ -3,13 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AlcoholController } from './alcohol.controller';
 import { AlcoholService } from './alcohol.service';
-import { CocktailModel, SpiritModel, WineModel } from './entities/alcohol.entity';
+import { AlcoholModel, SpiritModel, WineModel, CocktailModel } from './entities/alcohol.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { CommonModule } from 'src/common/common.module';
+import { ImageModel } from 'src/common/entities/image.entity';
 import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SpiritModel, WineModel, CocktailModel]), AuthModule, UsersModule, CommonModule],
+  imports: [
+    TypeOrmModule.forFeature([AlcoholModel, SpiritModel, WineModel, CocktailModel, ImageModel]),
+    AuthModule,
+    UsersModule,
+    CommonModule,
+  ],
   controllers: [AlcoholController],
   providers: [AlcoholService],
 })
