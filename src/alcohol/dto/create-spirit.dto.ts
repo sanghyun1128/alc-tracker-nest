63bc6d1ce7a7040fa464c4e15b62b3aa717a9749
sanghyun1128 <sanghyun1128@gmail.com>
2025-01-19 03:05:38 +0900

    feat: add optional images field to CreateSpiritDto with validation

--- src/alcohol/dto/create-spirit.dto.ts
+++ src/alcohol/dto/create-spirit.dto.ts
@@ -1,6 +1,8 @@
 import { PickType } from '@nestjs/mapped-types';
+import { IsOptional, IsString } from 'class-validator';
 
 import { SpiritModel } from '../entities/alcohol.entity';
+import { stringValidationMessage } from 'src/common/validation-message';
 
 export class CreateSpiritDto extends PickType(SpiritModel, [
   'name',
@@ -11,4 +13,11 @@ export class CreateSpiritDto extends PickType(SpiritModel, [
   'price',
   'purchaseLocation',
   'purchaseDate',
-] as const) {}
+] as const) {
+  @IsString({
+    each: true,
+    message: stringValidationMessage,
+  })
+  @IsOptional()
+  images: string[] = [];
+}
