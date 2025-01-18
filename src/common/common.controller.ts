21fa9199d49c065fefa85bfdafec848699d45d19
sanghyun1128 <sanghyun1128@gmail.com>
2025-01-19 03:03:28 +0900

    feat: add image upload endpoint with access token guard

--- src/common/common.controller.ts
+++ src/common/common.controller.ts
@@ -1,8 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 import { CommonService } from './common.service';
+import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
 
 @Controller('common')
 export class CommonController {
   constructor(private readonly commonService: CommonService) {}
+
+  @Post('image')
+  @UseGuards(AccessTokenGuard)
+  @UseInterceptors(FileInterceptor('image'))
+  postImage(@UploadedFile() image: Express.Multer.File) {
+    return {
+      fileName: image.filename,
+    };
+  }
 }
