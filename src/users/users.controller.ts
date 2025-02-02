fddfe2c87b58cceac01c179ee1cc29235d3baf96
sanghyun1128 <sanghyun1128@gmail.com>
2025-02-02 20:26:29 +0900

    feat: add user-specific endpoints for retrieving, updating, and deleting user information in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Put, UseGuards } from '@nestjs/common';
 
+import { User } from './decorator/user.decorator';
 import { UsersService } from './users.service';
+import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
 
 @Controller('users')
 export class UsersController {
@@ -10,5 +12,30 @@ export class UsersController {
   @Get('all')
   getAllUsers() {
     return this.usersService.getAllUsers();
+  }
+
+  @Get('/my')
+  @UseGuards(AccessTokenGuard)
+  getUserInfo(@User('id') userId: string) {
+    return this.usersService.getUserInfo(userId);
+  }
+
+  @Put('/my')
+  @UseGuards(AccessTokenGuard)
+  updateUserInfo(@User('id') userId: string) {
+    return this.usersService.updateUserInfo(userId);
+  }
+
+  @Delete('/my')
+  @UseGuards(AccessTokenGuard)
+  deleteUser(@User('id') userId: string) {
+    //TODO: delete all user data include reviews, alcohols
+    return this.usersService.deleteUser(userId);
+  }
+
+  @Get('/:userId')
+  @UseGuards(AccessTokenGuard)
+  getUserProfile(@Param('userId') userId: string) {
+    return this.usersService.getUserProfile(userId);
   }
 }
