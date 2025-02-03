e4f9aee2f7eace44db3b8c184578c26150a0cdcd
sanghyun1128 <sanghyun1128@gmail.com>
2025-02-03 21:24:17 +0900

    feat: implement user info retrieval and update functionality with error handling

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,11 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { QueryRunner, Repository } from 'typeorm';
 
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UserModel } from './entity/user.entity';
-import { ExistErrorMessage } from 'src/common/error-message';
+import { ExistErrorMessage, NotFoundErrorMessage } from 'src/common/error-message';
 
 @Injectable()
 export class UsersService {
@@ -12,9 +14,9 @@ export class UsersService {
     private readonly usersRepository: Repository<UserModel>,
   ) {}
 
-  async createUser(user: Pick<UserModel, 'nickname' | 'email' | 'password' | 'birth' | 'gender'>) {
+  async createUser(dto: CreateUserDto) {
     const nicknameExists = await this.usersRepository.findOne({
-      where: { nickname: user.nickname },
+      where: { nickname: dto.nickname },
     });
 
     if (nicknameExists) {
@@ -22,7 +24,7 @@ export class UsersService {
     }
 
     const emailExists = await this.usersRepository.findOne({
-      where: { email: user.email },
+      where: { email: dto.email },
     });
 
     if (emailExists) {
@@ -30,11 +32,7 @@ export class UsersService {
     }
 
     const newUser = this.usersRepository.create({
-      nickname: user.nickname,
-      email: user.email,
-      password: user.password,
-      birth: user.birth,
-      gender: user.gender,
+      ...dto,
     });
 
     return await this.usersRepository.save(newUser);
@@ -47,5 +45,82 @@ export class UsersService {
 
   async getUserByEmail(email: string) {
     return await this.usersRepository.findOne({ where: { email } });
+  }
+
+  async getUserInfo(userId: UserModel['id']) {
+    const user = await this.usersRepository.findOne({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new BadRequestException(NotFoundErrorMessage('user'));
+    }
+
+    return user;
+  }
+
+  async updateUserInfo(userId: UserModel['id'], dto: UpdateUserDto) {
+    const user = await this.usersRepository.findOne({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new BadRequestException(NotFoundErrorMessage('user'));
+    }
+
+    const nicknameExists = await this.usersRepository.findOne({
+      where: { nickname: dto.nickname },
+    });
+
+    if (nicknameExists) {
+      throw new BadRequestException(ExistErrorMessage('nickname'));
+    }
+
+    const updatedUser = await this.usersRepository.save({
+      ...user,
+      ...dto,
+    });
+
+    return updatedUser;
+  }
+
+  //TODO: backup user data before deletion
+  async deleteUser(userId: UserModel['id'], queryRunner: QueryRunner) {
+    const repository = queryRunner.manager.getRepository(UserModel);
+
+    const user = await repository.findOne({
+      where: { id: userId },
+    });
+
+    if (!user) {
+      throw new BadRequestException(NotFoundErrorMessage('user'));
+    }
+
+    for (const review of user.reviews) {
+      await repository.manager.remove(review);
+    }
+
+    for (const alcohol of user.alcohols) {
+      await repository.manager.remove(alcohol);
+    }
+
+    await repository.manager.remove(user.profileImage);
+
+    const result = await repository.delete({ id: userId });
+
+    return result;
+  }
+
+  async getUserProfile(userId: UserModel['id']) {
+    const user = await this.usersRepository.findOne({
+      where: { id: userId },
+      relations: ['profileImage', 'alcohols', 'reviews'],
+    });
+
+    if (!user) {
+      throw new BadRequestException(NotFoundErrorMessage('user'));
+    }
+
+    return user;
   }
 }
