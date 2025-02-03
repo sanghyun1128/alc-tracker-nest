eb03474d4891bb54c549ca36c38c519e3f7cdae1
sanghyun1128 <sanghyun1128@gmail.com>
2025-02-03 20:49:45 +0900

    feat: add CreateUserDto and UpdateUserDto for user data transfer

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,12 @@
+import { PickType } from '@nestjs/mapped-types';
+
+import { UserModel } from '../entity/user.entity';
+
+export class CreateUserDto extends PickType(UserModel, [
+  'nickname',
+  'email',
+  'password',
+  'birth',
+  'gender',
+  'comment',
+] as const) {}
