5254979ef6a646478c3ed4abdb7061839989ce61
sanghyun1128 <sanghyun1128@gmail.com>
2025-02-06 20:25:08 +0900

    refactor: remove unused imports from CreateUserDto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,6 @@
 import { PickType } from '@nestjs/mapped-types';
-import { IsOptional, IsString, Length } from 'class-validator';
 
 import { UserModel } from '../entity/user.entity';
-import { stringValidationMessage, lengthValidationMessage } from 'src/common/validation-message';
 
 export class CreateUserDto extends PickType(UserModel, [
   'nickname',
