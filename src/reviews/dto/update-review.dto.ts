98eab63fc85f8fc024dd43d925f57d3212fd5d8b
sanghyun1128 <sanghyun1128@gmail.com>
2025-02-11 14:11:04 +0900

    refactor: remove unused AlcoholModel import from UpdateReviewDto

--- src/reviews/dto/update-review.dto.ts
+++ src/reviews/dto/update-review.dto.ts
@@ -1,7 +1,6 @@
 import { IsEnum, IsString } from 'class-validator';
 
 import { AlcoholType } from 'src/alcohol/const/alcohol-type.const';
-import { AlcoholModel } from 'src/alcohol/entity/alcohol.entity';
 import { enumValidationMessage, stringValidationMessage } from 'src/common/validation-message';
 import { UserModel } from 'src/users/entity/user.entity';
 
