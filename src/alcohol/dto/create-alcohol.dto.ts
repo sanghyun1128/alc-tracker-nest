e2c73e612f0826ea34086544673ce4935852b458
sanghyun1128 <sanghyun1128@gmail.com>
2025-02-13 14:51:35 +0900

    feat: add alcoholType property with validation to CreateAlcoholDto

--- src/alcohol/dto/create-alcohol.dto.ts
+++ src/alcohol/dto/create-alcohol.dto.ts
@@ -1,6 +1,14 @@
-import { IsOptional } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
+
+import { AlcoholType } from '../const/alcohol-type.const';
+import { stringValidationMessage } from 'src/common/validation-message';
 
 export class CreateAlcoholDto {
+  @IsEnum({
+    message: stringValidationMessage,
+  })
+  alcoholType: AlcoholType;
+
   @IsOptional()
   images: {
     path: string;
