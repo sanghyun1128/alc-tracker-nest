18287976f9bb668a185356949495f76ed3609f80
sanghyun1128 <sanghyun1128@gmail.com>
2025-02-13 14:53:02 +0900

    feat: move image processing part to AlcoholService

--- src/alcohol/alcohol.service.ts
+++ src/alcohol/alcohol.service.ts
@@ -25,8 +25,8 @@ import {
   AlcoholModel,
 } from 'src/alcohol/entity/alcohol.entity';
 import { CommonService } from 'src/common/common.service';
-import { BaseModel } from 'src/common/entity/base.entity';
 import { NotFoundErrorMessage, PermissionErrorMessage } from 'src/common/error-message';
+import { ReviewsService } from 'src/reviews/reviews.service';
 import { UserModel } from 'src/users/entity/user.entity';
 
 @Injectable()
@@ -42,6 +42,7 @@ export class AlcoholService {
     private readonly cocktailRepository: Repository<CocktailModel>,
 
     private readonly commonService: CommonService,
+    private readonly reviewService: ReviewsService,
   ) {}
 
   private repositoryMap = {
@@ -58,61 +59,7 @@ export class AlcoholService {
     cocktail: CocktailModel,
   };
 
-  async getUserAlcohols(
-    type: AlcoholType,
-    userId: UserModel['id'],
-    dto: PaginateAlcoholDto,
-  ): Promise<
-    | { data: BaseModel[]; total: number }
-    | { data: BaseModel[]; cursor: { after: number }; count: number; next: URL }
-  > {
-    const repository = this.commonService.getRepositoryWithQueryRunner(
-      type,
-      this.repositoryMap,
-      this.modelMap,
-    );
-
-    return this.commonService.paginate(
-      dto,
-      repository,
-      {
-        where: {
-          owner: {
-            id: userId,
-          },
-        },
-        relations: ['owner', 'images'],
-      },
-      `alcohol/${type}`,
-    );
-  }
-
-  async createAlcohol(
-    type: AlcoholType,
-    userId: string,
-    dto: CreateSpiritDto | CreateWineDto | CreateCocktailDto,
-    queryRunner?: QueryRunner,
-  ): Promise<AlcoholModel> {
-    const repository = this.commonService.getRepositoryWithQueryRunner(
-      type,
-      this.repositoryMap,
-      this.modelMap,
-      queryRunner,
-    ) as Repository<AlcoholModel>;
-
-    const alcohol = repository.create({
-      owner: {
-        id: userId,
-      },
-      ...dto,
-      images: [],
-    });
-
-    const result = await repository.save(alcohol);
-
-    return result;
-  }
-
+  // Get alcohol which is specified by id
   async getAlcoholById(alcoholId: string, queryRunner?: QueryRunner): Promise<AlcoholModel> {
     const repository = this.commonService.getRepositoryWithQueryRunner(
       'alcohol',
@@ -135,11 +82,86 @@ export class AlcoholService {
     return alcohol;
   }
 
+  // Get all alcohols of a user
+  async getUserAlcohols(
+    type: AlcoholType,
+    userId: UserModel['id'],
+    dto: PaginateAlcoholDto,
+  ): Promise<
+    | { data: AlcoholModel[]; total: number }
+    | { data: AlcoholModel[]; cursor: { after: number }; count: number; next: URL }
+  > {
+    const repository = this.commonService.getRepositoryWithQueryRunner(
+      type,
+      this.repositoryMap,
+      this.modelMap,
+    );
+
+    return this.commonService.paginate(
+      dto,
+      repository,
+      {
+        where: {
+          owner: {
+            id: userId,
+          },
+        },
+        relations: ['owner'],
+      },
+      `alcohol/${userId}/${type}`,
+    );
+  }
+
+  // Create a new alcohol with specified type
+  async createAlcohol(
+    userId: string,
+    dto: CreateSpiritDto | CreateWineDto | CreateCocktailDto,
+    queryRunner?: QueryRunner,
+  ): Promise<AlcoholModel> {
+    // 1. Get repository with queryRunner
+    const repository = this.commonService.getRepositoryWithQueryRunner(
+      dto.alcoholType,
+      this.repositoryMap,
+      this.modelMap,
+      queryRunner,
+    ) as Repository<AlcoholModel>;
+
+    const { images, ...dtoWithoutImages } = dto;
+
+    // 2. Create alcohol
+    const alcohol = repository.create({
+      owner: {
+        id: userId,
+      },
+      ...dtoWithoutImages,
+      images: [],
+    });
+
+    const result = await repository.save(alcohol);
+
+    // 3. Create images
+    for (const image of images) {
+      image.isNew &&
+        (await this.commonService.createImage(
+          {
+            alcoholId: alcohol.id,
+            order: image.order,
+            path: image.path,
+          },
+          queryRunner,
+        ));
+    }
+
+    return this.getAlcoholById(result.id, queryRunner);
+  }
+
+  // Delete alcohol which is specified by id
   async deleteAlcoholById(
     alcoholId: string,
     userId: string,
     queryRunner?: QueryRunner,
   ): Promise<DeleteResult> {
+    // 1. Get repository with queryRunner
     const repository = this.commonService.getRepositoryWithQueryRunner(
       'alcohol',
       this.repositoryMap,
@@ -147,12 +169,8 @@ export class AlcoholService {
       queryRunner,
     ) as Repository<AlcoholModel>;
 
-    const alcohol = await repository.findOne({
-      where: {
-        id: alcoholId,
-      },
-      relations: ['owner', 'images', 'reviews'],
-    });
+    // 2. Get alcohol by id and check if it exists and the user is the owner
+    const alcohol = await this.getAlcoholById(alcoholId, queryRunner);
 
     if (!alcohol) {
       throw new NotFoundException(NotFoundErrorMessage('alcohol'));
@@ -162,19 +180,28 @@ export class AlcoholService {
       throw new BadRequestException(PermissionErrorMessage('alcohol', 'delete'));
     }
 
+    // 3. Delete related images
     for (const image of alcohol.images) {
       await this.commonService.deleteImageById(image.id, queryRunner);
     }
 
+    // 4. Delete related reviews
+    for (const review of alcohol.reviews) {
+      await this.reviewService.deleteReviewById(review.id, userId, queryRunner);
+    }
+
+    // 5. Delete alcohol
     return await repository.delete(alcoholId);
   }
 
+  // Update alcohol which is specified by id
   async updateAlcohol(
     alcoholId: string,
     userId: string,
     dto: UpdateSpiritDto | UpdateWineDto | UpdateCocktailDto,
     queryRunner?: QueryRunner,
   ): Promise<AlcoholModel> {
+    // 1. Get repository with queryRunner
     const repository = this.commonService.getRepositoryWithQueryRunner(
       dto.type,
       this.repositoryMap,
@@ -182,12 +209,8 @@ export class AlcoholService {
       queryRunner,
     ) as Repository<AlcoholModel>;
 
-    const alcohol = await repository.findOne({
-      where: {
-        id: alcoholId,
-      },
-      relations: ['owner', 'images'],
-    });
+    // 2. Get alcohol by id and check if it exists and the user is the owner
+    const alcohol = await this.getAlcoholById(alcoholId, queryRunner);
 
     if (!alcohol) {
       throw new NotFoundException(NotFoundErrorMessage(dto.type));
@@ -197,6 +220,7 @@ export class AlcoholService {
       throw new BadRequestException(PermissionErrorMessage(dto.type, 'update'));
     }
 
+    // 3. Update alcohol
     const { images, ...dtoWithoutImages } = dto;
 
     const updatedAlcohol = await repository.save({
@@ -204,7 +228,44 @@ export class AlcoholService {
       ...dtoWithoutImages,
     });
 
-    return updatedAlcohol;
+    // 4. Update images
+    for (const image of images) {
+      // 4-1. If the image is new, create a new image
+      if (image.isNew) {
+        await this.commonService.createImage(
+          {
+            alcoholId: alcohol.id,
+            order: image.order,
+            path: image.path,
+          },
+          queryRunner,
+        );
+      } else {
+        // 4-2. If the image is not new, update the existing image
+        const alcoholImage = alcohol.images.find(
+          (alcoholImage) => alcoholImage.path === image.path,
+        );
+
+        if (alcoholImage) {
+          await this.commonService.updateImage(
+            alcoholImage.id,
+            {
+              order: image.order,
+            },
+            queryRunner,
+          );
+        }
+      }
+    }
+
+    // 5. Delete unused images
+    for (const image of dto.deletedImages) {
+      const imageId = alcohol.images.find((alcoholImage) => alcoholImage.path === image.path).id;
+
+      await this.commonService.deleteImageById(imageId, queryRunner);
+    }
+
+    return await this.getAlcoholById(updatedAlcohol.id, queryRunner);
   }
 
   //TODO: Test code
@@ -214,7 +275,8 @@ export class AlcoholService {
     for (let i = 0; i < 100; i++) {
       purchaseDate.setDate(purchaseDate.getDate() - 1);
 
-      await this.createAlcohol(AlcoholType.SPIRIT, userId, {
+      await this.createAlcohol(userId, {
+        alcoholType: AlcoholType.SPIRIT,
         name: `Test Spirit ${i}`,
         category: SpiritCategoryEnum[i % 17],
         cask: CaskEnum[i % 9],
@@ -226,7 +288,8 @@ export class AlcoholService {
         images: [],
       });
 
-      await this.createAlcohol(AlcoholType.WINE, userId, {
+      await this.createAlcohol(userId, {
+        alcoholType: AlcoholType.WINE,
         name: `Test Wine ${i}`,
         category: WineCategoryEnum[i % 5],
         region: WineRegionEnum[i % 5],
@@ -241,7 +304,8 @@ export class AlcoholService {
         images: [],
       });
 
-      await this.createAlcohol(AlcoholType.COCKTAIL, userId, {
+      await this.createAlcohol(userId, {
+        alcoholType: AlcoholType.COCKTAIL,
         name: `Test Cocktail ${i}`,
         category: CocktailCategoryEnum[i % 5],
         images: [],
