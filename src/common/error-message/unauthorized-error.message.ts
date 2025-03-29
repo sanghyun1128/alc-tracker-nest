4e6d88813012c80125a2cdd6448f21c4d1fb1b50
sanghyun1128 <sanghyun1128@gmail.com>
2025-03-29 15:28:19 +0900

    feat: add new unauthorized error messages for invalid access and refresh tokens

--- src/common/error-message/unauthorized-error.message.ts
+++ src/common/error-message/unauthorized-error.message.ts
@@ -4,4 +4,6 @@
 export const enum UnauthorizedErrorMessage {
   EmailOrPasswordIsIncorrect = 'Email or password is incorrect',
   InvalidToken = 'Invalid token',
+  InvalidAccessToken = 'Invalid access token',
+  InvalidRefreshToken = 'Invalid refresh token',
 }
